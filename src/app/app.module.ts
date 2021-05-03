430552e2164b50337d06162f1a119078cae6716b
nikolinavukovic <nikolina.vukovic@uns.ac.rs>
2021-05-03 18:12:10 +0200

    Vezbe 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VoziloComponent } from './components/primer-components/vozilo/vozilo.component';
+import { AutomobilComponent } from './components/primer-components/automobil/automobil.component';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { HomeComponent } from './components/core/home/home.component';
+import { AuthorComponent } from './components/core/author/author.component';
+import { AboutComponent } from './components/core/about/about.component';
+import { KreditComponent } from './components/kredit/kredit.component';
+import { KlijentComponent } from './components/klijent/klijent.component';
+import { TipRacunaComponent } from './components/tip-racuna/tip-racuna.component';
+import { RacunComponent } from './components/racun/racun.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VoziloComponent,
+    AutomobilComponent,
+    HomeComponent,
+    AuthorComponent,
+    AboutComponent,
+    KreditComponent,
+    KlijentComponent,
+    TipRacunaComponent,
+    RacunComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatGridListModule,
+    MatExpansionModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
